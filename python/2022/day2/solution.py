c28a46974d08d911c9aa986276e0fca215d5d233
Adrien-ANTON-LUDWIG <ludwig.adrien0@gmail.com>
2022-12-08 16:28:11 +0100

    Add 2022/02 part 2 (hardcoded)
    Again, given the little number of outcomes, we also hardcoded the
    scores of the possible rounds for part 2.
    Changed the constants X, Y, Z to ROCK, PAPER, SCISSORS because part 2
    changed the meaning of the letters.

--- python/2022/day2/solution.py
+++ python/2022/day2/solution.py
@@ -5,21 +5,35 @@ LOSE = 0
 DRAW = 3
 WIN = 6
 
-A = X = 1  # Rock
-B = Y = 2  # Paper
-C = Z = 3  # Scissors
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
 
-# Create a dictionary of all possible rounds and their scores
-POSSIBLE_ROUNDS = {
-    "A X": X + DRAW, # Rock vs Rock
-    "A Y": Y + WIN, # Rock vs Paper
-    "A Z": Z + LOSE, # Rock vs Scissors
-    "B X": X + LOSE, # Paper vs Rock
-    "B Y": Y + DRAW, # Paper vs Paper
-    "B Z": Z + WIN, # Paper vs Scissors 
-    "C X": X + WIN, # Scissors vs Rock
-    "C Y": Y + LOSE, # Scissors vs Paper
-    "C Z": Z + DRAW, # Scissors vs Scissors
+# Create a dictionary of all possible rounds and their scores for part 1
+# X = Rock, Y = Paper, Z = Scissors
+POSSIBLE_ROUNDS_1 = {
+    "A X": ROCK + DRAW, # Rock vs Rock
+    "A Y": PAPER + WIN, # Rock vs Paper
+    "A Z": SCISSORS + LOSE, # Rock vs Scissors
+    "B X": ROCK + LOSE, # Paper vs Rock
+    "B Y": PAPER + DRAW, # Paper vs Paper
+    "B Z": SCISSORS + WIN, # Paper vs Scissors 
+    "C X": ROCK + WIN, # Scissors vs Rock
+    "C Y": PAPER + LOSE, # Scissors vs Paper
+    "C Z": SCISSORS + DRAW, # Scissors vs Scissors
+}
+
+# Create a dictionary of all possible rounds and their scores for part 2
+POSSIBLE_ROUNDS_2 = {
+    "A X": LOSE + SCISSORS, # Lose against Rock, play Scissors
+    "A Y": DRAW + ROCK, # Draw against Rock, play Rock
+    "A Z": WIN + PAPER, # Win against Rock, play Paper
+    "B X": LOSE + ROCK, # Lose against Paper, play Rock
+    "B Y": DRAW + PAPER, # Draw against Paper, play Paper
+    "B Z": WIN + SCISSORS, # Win against Paper, play Scissors
+    "C X": LOSE + PAPER, # Lose against Scissors, play Paper
+    "C Y": DRAW + SCISSORS, # Draw against Scissors, play Scissors
+    "C Z": WIN + ROCK, # Win against Scissors, play Rock
 }
 
 
@@ -32,12 +46,14 @@ if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         input = [line.strip() for line in f.readlines()]
 
-    # Part 1: Calculate the total score
-    score = 0
+    # Calculate the total score for each part (with different rules)
+    score1 = 0
+    score2 = 0
 
     # For each round, add the score to the total score
     for line in input:
-        score += POSSIBLE_ROUNDS[line]
+        score1 += POSSIBLE_ROUNDS_1[line]
+        score2 += POSSIBLE_ROUNDS_2[line]
 
-    print(score)
-    print("TODO part 2")
+    print(score1)
+    print(score2)
