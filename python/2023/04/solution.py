56ce4e46dd306ec075894a3baad236c7f0331618
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-04 23:45:58 +0100

    Add 2023/04 part 2

--- python/2023/04/solution.py
+++ python/2023/04/solution.py
@@ -11,17 +11,31 @@ if __name__ == "__main__":
     with open(sys.argv[1]) as f:
         input = [line.strip() for line in f.readlines()]
 
+    # Part 1
     points_sum = 0
 
-    for card in input:
+    # Part 2
+    cards_count = [1] * len(input)
+    total_nb_cards = 0
+
+    for index, card in enumerate(input):
         _, numbers = card.split(":")
         winning_numbers, my_numbers = numbers.split("|")
         winning_numbers = set(map(int, winning_numbers.split()))
         my_numbers = set(map(int, my_numbers.split()))
-        points = winning_numbers.intersection(my_numbers)
+        my_winning_numbers_count = len(winning_numbers.intersection(my_numbers))
 
-        if len(points):
-            points_sum += 2 ** (len(points) - 1)
+        # Part 1
+        if my_winning_numbers_count:
+            points_sum += 2 ** (my_winning_numbers_count - 1)
+
+        # Part 2
+        total_nb_cards += cards_count[index]
+        for offset in range(1, my_winning_numbers_count + 1):
+            cards_count[index + offset] += cards_count[index]
 
     # Part 1
     print(points_sum)
+
+    # Part 2
+    print(total_nb_cards)
