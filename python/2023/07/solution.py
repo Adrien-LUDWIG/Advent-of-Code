1e472202b4c0d274f438d24f07bedfb739df4942
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-07 11:45:03 +0100

    Add 2023/07 part 2

--- python/2023/07/solution.py
+++ python/2023/07/solution.py
@@ -1,13 +1,19 @@
 import sys
 from collections import Counter
 
-CARD_TO_RANK = {card: index for index, card in enumerate("23456789TJQKA")}
+CARD_TO_RANK = {card: index for index, card in enumerate("J23456789TQKA")}
 
 
 def key_hands_bids(hand_bid):
     hand, _ = hand_bid
 
-    cards_count = list(zip(*Counter(hand).most_common()))[1]
+    # Handle jokers, count them as the most common element (that is not joker)
+    counter = Counter(hand)
+    joker_count = counter["J"]
+    counter["J"] = 0
+    counter[counter.most_common(1)[0][0]] += joker_count
+
+    cards_count = list(zip(*counter.most_common()))[1]
     cards_ranks = [CARD_TO_RANK[card] for card in hand]
 
     return (cards_count, cards_ranks)
@@ -29,5 +35,5 @@ if __name__ == "__main__":
 
     hands_sorted, bids_sorted = zip(*sorted(zip(hands, bids), key=key_hands_bids))
 
-    # Part 1
+    # Part 2
     print(sum([(rank + 1) * bid for rank, bid in enumerate(bids_sorted)]))
