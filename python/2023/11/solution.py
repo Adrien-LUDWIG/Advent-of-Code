dcefc09e3f20365f362b59b5405c8e7bac7512b2
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-11 21:06:50 +0100

    Add 2023/11 part 2

--- python/2023/11/solution.py
+++ python/2023/11/solution.py
@@ -1,18 +1,45 @@
 import sys
 
 
-def expand(image):
-    for index in range(len(image) - 1, -1, -1):
+def get_fake_indices(image, expansion):
+    fake_row_indices = []
+    fake_index = 0
+
+    for index in range(len(image)):
         if all(cell == "." for cell in image[index]):
-            image.insert(index, image[index])
+            fake_index += expansion
+        else:
+            fake_index += 1
+        fake_row_indices.append(fake_index)
 
-    image_transposed = list(zip(*image))
+    return fake_row_indices
 
-    for index in range(len(image_transposed) - 1, -1, -1):
-        if all(cell == "." for cell in image_transposed[index]):
-            image_transposed.insert(index, image_transposed[index])
 
-    return list(zip(*image_transposed))
+def expand(image, expansion):
+    fake_row_indices = get_fake_indices(image, expansion)
+    fake_col_indices = get_fake_indices(list(zip(*image)), expansion)
+
+    return fake_row_indices, fake_col_indices
+
+
+def distance_sum(image, expansion):
+    fake_row_indices, fake_col_indices = expand(image, expansion)
+
+    galaxies_positions = [
+        (fake_i, fake_j)
+        for real_i, fake_i in enumerate(fake_row_indices)
+        for real_j, fake_j in enumerate(fake_col_indices)
+        if image[real_i][real_j] == "#"
+    ]
+
+    total_distance = 0
+    for i in range(len(galaxies_positions)):
+        for j in range(i + 1, len(galaxies_positions)):
+            x1, y1 = galaxies_positions[i]
+            x2, y2 = galaxies_positions[j]
+            total_distance += abs(x1 - x2) + abs(y1 - y2)
+
+    return total_distance
 
 
 if __name__ == "__main__":
@@ -24,22 +51,10 @@ if __name__ == "__main__":
 
     # Read input file and split into stripped lines
     with open(sys.argv[1]) as f:
-        input = [line.strip() for line in f.readlines()]
+        image = [line.strip() for line in f.readlines()]
 
-    expanded = expand(input)
+    # Part 1
+    print(distance_sum(image, expansion=2))
 
-    galaxies_positions = [
-        (i, j)
-        for i in range(len(expanded))
-        for j in range(len(expanded[0]))
-        if expanded[i][j] == "#"
-    ]
-
-    total_distance = 0
-    for i in range(len(galaxies_positions)):
-        for j in range(i + 1, len(galaxies_positions)):
-            x1, y1 = galaxies_positions[i]
-            x2, y2 = galaxies_positions[j]
-            total_distance += abs(x1 - x2) + abs(y1 - y2)
-
-    print(total_distance)
+    # Part 2
+    print(distance_sum(image, expansion=1000000))
