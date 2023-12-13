e2c19ddf0b0a6495fed9995c746679efa44ee6d0
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-13 19:35:35 +0100

    Add 2023/09 part 1

---
+++ python/2023/09/solution.py
@@ -0,0 +1,24 @@
+# Solved on smartphone using Pydroid
+
+# TODO: Replace copy/paste input string here
+input = <input_str>
+
+input = [list(map(int, line.split())) for line in input.split("\n")]
+
+def find_next(seq):
+    stack = [seq]
+    
+    while any(seq):
+        stack.append([rhs - lhs for lhs, rhs in zip(seq[:-1], seq[1:])])
+        seq = stack[-1]
+     
+    prediction = 0
+    
+    while stack:
+        seq = stack.pop()
+        prediction += seq[-1]
+        
+    return prediction
+        
+    
+print(sum(find_next(seq) for seq in input))
