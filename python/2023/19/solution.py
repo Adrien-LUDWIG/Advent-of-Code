42e7c1b97c233c0be4ac21ba5e81154c584b8190
Adrien-LUDWIG <ludwig.adrien0@gmail.com>
2023-12-20 00:36:38 +0100

    Add 2023/19 part 2

--- python/2023/19/solution.py
+++ python/2023/19/solution.py
@@ -2,6 +2,8 @@ import operator
 import sys
 import re
 from aocd import data
+from copy import deepcopy
+from math import prod
 
 CATEGORIES = {
     "x": 0,
@@ -67,14 +69,65 @@ def part1(rules, parts):
     return total
 
 
-def part2(data):
-    pass
+def update_combination(combination, test, invert=False):
+    category, operation, value, _ = test
+
+    if invert:
+        if operation == operator.gt:
+            operation = operator.lt
+            value += 1
+        else:  # operation == operator.lt
+            operation = operator.gt
+            value -= 1
+
+    if operation == operator.gt:
+        combination[category][0] = max(combination[category][0], value + 1)
+    else:  # operation == operator.lt
+        combination[category][1] = min(combination[category][1], value - 1)
+
+    return combination
+
+
+def accepted_combinations(rules):
+    combinations = []
+
+    def dfs(rule, combination):
+        if rule == "A":
+            combinations.append(combination)
+            return
+        if rule == "R":
+            return
+
+        reversed_combination = deepcopy(combination)
+
+        for test in rules[rule][:-1]:
+            _, _, _, next_rule = test
+            new_combination = deepcopy(reversed_combination)
+            new_combination = update_combination(new_combination, test)
+            dfs(next_rule, new_combination)
+
+            reversed_combination = update_combination(reversed_combination, test, True)
+
+        # Default
+        dfs(rules[rule][-1][-1], reversed_combination)
+
+    dfs("in", [[1, 4000] for _ in range(4)])
+    return combinations
+
+
+def part2(rules):
+    combinations = accepted_combinations(rules)
+    return sum(
+        [
+            prod([max - min + 1 for min, max in combination])
+            for combination in combinations
+        ]
+    )
 
 
 def solve(data):
     rules, parts = parse_data(data)
-
-    return part1(rules, parts), part2(data)
+    return part1(rules, parts), part2(rules)
 
 
 if __name__ == "__main__":
